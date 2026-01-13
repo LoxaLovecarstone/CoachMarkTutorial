921a8fdcf3bae9bfc4ee597d987a95d74a95292d
LOVECARSTONE\kdc62 <kdc6208@naver.com>
2026-01-13 11:22:02 +0900

    HOTFIX: Wrong Spelling

--- app/src/main/java/com/example/coachmarktutorial/ui/main/MainScreen.kt
+++ app/src/main/java/com/example/coachmarktutorial/ui/main/MainScreen.kt
@@ -12,7 +12,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.materiala3.Icon
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
